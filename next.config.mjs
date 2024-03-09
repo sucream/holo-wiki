880663063a019d6f2dd32261f1bf287b13c3441d
Namsoo Hong <ghdskatnghds@gmail.com>
2024-03-09 22:18:37 +0900

    Feat/skeleton (#2)
    * 기본적인 위키 틀 생성
    
    * useSearchParams 관련 Suspense 처리 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+        port: '',
+        pathname: '/vi/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'yt3.ggpht.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'schedule-static.hololive.tv',
+        port: '',
+        pathname: '/image/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
---
+++ src/lib/ReactQueryProvider.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
+
+type Props = Readonly<{
+  children: React.ReactNode;
+}>;
+
+export default function ReactQueryProvider(props: Props) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {props.children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
