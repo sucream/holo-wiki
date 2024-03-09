36341e4251dc656b4b93b49854ff4499aee9fb3d
sucream <ghdskatnghds@gmail.com>
2024-03-09 22:14:53 +0900

    useSearchParams 관련 Suspense 처리 추가

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,11 +1,14 @@
 // import { Suspense } from "react";
 import Schedule from "./_components/Schedule";
+import { Suspense } from "react";
 
 export default function SchedulePage() {
   return (
     <div>
       <h1>Holo Schedule</h1>
-      <Schedule />
+      <Suspense>
+        <Schedule />
+      </Suspense>
     </div>
   );
 }
